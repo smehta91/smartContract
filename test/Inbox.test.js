1143978190530604f8fa6e6b504f2f374652ab78
Shrey Mehta <shrey@dream11.com>
2018-05-08 11:36:41 +0530

    get update tests added

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,17 +5,28 @@ const {interface, bytecode} = require('../compile')
 
 const web3 = new Web3(ganache.provider());
 let accounts, inbox;
-
+const initString = 'Hello Blockchain'
 beforeEach(async () => {
 	// get a list of all accounts
 	accounts = await web3.eth.getAccounts();
 	inbox = await new web3.eth.Contract(JSON.parse(interface))
-		.deploy({data: bytecode, arguments: ['Hello Blockchain']})
+		.deploy({data: bytecode, arguments: [initString]})
 		.send({ from: accounts[0], gas: '1000000' })
 	//use one of the account to deploy the contract
 })
 describe('Inbox', () => {
-	it('deploys a contract', ()=>{
-		console.log(inbox)
+	it('deploys a contract', () => {
+		assert.ok(inbox.options.address);
+	});
+
+	it('has a default message', async () => {
+		const message = await inbox.methods.message().call()
+		assert.equal(message, initString)
+	});
+
+	it('can change the message', async () => {
+		await inbox.methods.setMessage('init transaction').send({ from: accounts[0] })
+		const message = await inbox.methods.message().call()
+		assert.equal(message, 'init transaction')
 	})
 })
